dbf2ddd631eab20fd9eca3172cb15c59870d7d9f
AuroVarat <aurovaratpatnaik@gmail.com>
2025-03-12 16:32:11 +0000

    Some more organizing

--- run.py
+++ run.py
@@ -17,18 +17,18 @@ def run_boltz():
     notify_end("test Boltz predict started")
     file = "train_fasta_subset"
     command = ["boltz", "predict", file , "--use_msa_server"]
-    # result = subprocess.run(command, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True)
 
-    # if result.returncode == 0:
-    #     msg = "Boltz predict ran successfully"
-    #     print(msg)
-    #     notify_end(msg)
+    if result.returncode == 0:
+        msg = "Boltz predict ran successfully"
+        print(msg)
+        notify_end(msg)
         
-    # else:
-    #     msg = "Error running boltz predict "
-    #     err = result.stderr
-    #     print(result.stderr)
-    #     notify_end(msg+err)
+    else:
+        msg = "Error running boltz predict "
+        err = result.stderr
+        print(result.stderr)
+        notify_end(msg+err)
         
         
 if __name__ == "__main__":
